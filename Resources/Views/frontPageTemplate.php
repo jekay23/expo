2c1800ec323bc2c19111587abd1aad5d9101afac
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-16 18:41:08 +0700

    T-3.1 - frontPageTemplate - Add basic footer, modify header

--- Resources/Views/frontPageTemplate.php
+++ Resources/Views/frontPageTemplate.php
@@ -1,29 +1,44 @@
-<header class="navbar navbar-expand-md navbar-dark bg-success" role="navigation">
-    <div class="collapse navbar-collapse container align-self-center">
-        <a class="navbar-brand" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
-            <object type="image/svg+xml" data="image/logoMMD.svg" width="100" height="65"></object>
-        </a>
-        <a class="navbar-brand px-lg-5" href="/">
-            <strong>Выставка фотографов мехмата</strong>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- TODO commit when something works <img src="/image/favicons.ico" alt="Логотип ММФ">-->
-        <div class="collapse navbar-collapse justify-content-sm-evenly" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item px-xl-5">
-                    <a class="nav-link active" href="#">Лента</a>
-                </li>
-                <li class="nav-item px-lg-5">
-                    <a class="nav-link" href="#">Отбор</a>
-                </li>
-                <li class="nav-item px-xl-5">
-                    <a class="nav-link" href="#">Профиль</a>
-                </li>
-            </ul>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top" role="navigation" style="height: 6vh; min-height: 60px">
+        <div class="collapse navbar-collapse container-md align-self-center">
+            <a class="navbar-brand" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
+                <img src="/image/logoMMD.png" alt="Лого ММФ" style="height: 5vh; min-height: 55px">
+            </a>
+            <a class="navbar-brand px-lg-5" href="/">
+                <strong>Выставка фотографов мехмата</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- TODO commit when something works <img src="/image/favicons.ico" alt="Логотип ММФ">-->
+            <div class="collapse navbar-collapse justify-content-sm-evenly" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item px-xl-5">
+                        <a class="nav-link active" href="#">Лента</a>
+                    </li>
+                    <li class="nav-item px-lg-5">
+                        <a class="nav-link" href="#">Отбор</a>
+                    </li>
+                    <li class="nav-item px-xl-5">
+                        <a class="nav-link" href="#">Профиль</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+    </nav>
 </header>
-Here you will be able to see the front page of Expo MMD website.
+
+<main class="container" role="main" style="padding-top: 70px">
+    <div>Here you will be able to see the front page of Expo MMD website.</div>
+</main>
+
+<footer class="footer mt-auto py-3 bg-light fixed-bottom">
+    <div class="container">
+        <ul class="nav justify-content-center">
+            <li class="nav-item"><a href="#" class="nav-link px-5 text-muted">Поддержка</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-5 text-muted">Пользовательское соглашение</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-5 text-muted">FAQ</a></li>
+        </ul>
+    </div>
+</footer>
