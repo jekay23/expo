f47028323ad97d3ceb470524ff4fe8957111dbd1
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-16 17:31:47 +0700

    T-3.1 - mainTemplate - Add Bootstrap importing

--- Resources/Views/mainTemplate.php
+++ Resources/Views/mainTemplate.php
@@ -2,9 +2,11 @@
 <html lang="ru">
     <head>
         <title><?php echo $title?></title>
-        <link rel="icon" type="image/x-icon" href="../../Public/Images/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="/image/favicon.ico">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <?php require $template?>
     </body>
 </html>
