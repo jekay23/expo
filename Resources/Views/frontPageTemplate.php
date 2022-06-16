34052ed9d4667a1a7f241bdeb11e2d8f8fbb725e
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-16 17:32:51 +0700

    T-3.1 - frontPageTemplate - Add basic header

--- Resources/Views/frontPageTemplate.php
+++ Resources/Views/frontPageTemplate.php
@@ -1,1 +1,29 @@
+<header class="navbar navbar-expand-md navbar-dark bg-success" role="navigation">
+    <div class="collapse navbar-collapse container align-self-center">
+        <a class="navbar-brand" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
+            <object type="image/svg+xml" data="image/logoMMD.svg" width="100" height="65"></object>
+        </a>
+        <a class="navbar-brand px-lg-5" href="/">
+            <strong>Выставка фотографов мехмата</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- TODO commit when something works <img src="/image/favicons.ico" alt="Логотип ММФ">-->
+        <div class="collapse navbar-collapse justify-content-sm-evenly" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item px-xl-5">
+                    <a class="nav-link active" href="#">Лента</a>
+                </li>
+                <li class="nav-item px-lg-5">
+                    <a class="nav-link" href="#">Отбор</a>
+                </li>
+                <li class="nav-item px-xl-5">
+                    <a class="nav-link" href="#">Профиль</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
 Here you will be able to see the front page of Expo MMD website.
