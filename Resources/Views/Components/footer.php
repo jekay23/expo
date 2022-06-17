7a1252d6900cbf6092a0506bbcf46484b2418a52
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-17 12:02:13 +0700

    T-2.1 - Templates - Improve header and footer

--- Resources/Views/Components/footer.php
+++ Resources/Views/Components/footer.php
@@ -1,9 +1,9 @@
-<footer class="footer mt-auto py-3 fixed-bottom" style="background-color: #232222">
+<footer class="footer mt-5 py-3" style="background-color: #232222">
     <div class="container">
         <ul class="nav justify-content-center">
-            <li class="nav-item"><a href="#" class="nav-link px-5 text-muted">Поддержка</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-5 text-muted">Пользовательское соглашение</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-5 text-muted">FAQ</a></li>
+            <li class="nav-item px-5"><a href="#" class="nav-link text-muted">Поддержка</a></li>
+            <li class="nav-item px-5"><a href="#" class="nav-link text-muted">Пользовательское соглашение</a></li>
+            <li class="nav-item px-5"><a href="#" class="nav-link text-muted">FAQ</a></li>
         </ul>
     </div>
 </footer>
--- Resources/Views/Components/header.php
+++ Resources/Views/Components/header.php
@@ -1,29 +1,31 @@
 <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top" role="navigation" style="height: 6vh; min-height: 60px; background-color: #54b686">
-        <div class="collapse navbar-collapse container-md align-self-center">
-            <a class="navbar-brand" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
-                <img src="/image/logoMMD.png" alt="Лого ММФ" style="height: 5vh; min-height: 55px">
-            </a>
-            <a class="navbar-brand px-lg-5" href="/">
-                <strong>Выставка фотографов мехмата</strong>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top" role="navigation"
+         style="height: 6vh; min-height: 60px; background-color: #54b686">
+        <div class="container-md align-self-center">
+            <a class="navbar-brand py-0" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
+                <img src="/image/logoMMD.png" alt="Логотип ММФ" style="height: 5vh; min-height: 55px">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <!-- TODO commit when something works <img src="/image/favicons.ico" alt="Логотип ММФ">-->
-            <div class="collapse navbar-collapse justify-content-sm-evenly" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item px-xl-5">
-                        <a class="nav-link active" href="#">Лента</a>
-                    </li>
-                    <li class="nav-item px-lg-5">
-                        <a class="nav-link" href="#">Отбор</a>
-                    </li>
-                    <li class="nav-item px-xl-5">
-                        <a class="nav-link" href="#">Профиль</a>
-                    </li>
-                </ul>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <a class="navbar-brand px-lg-5" href="/" id="navbarSupportedContent">
+                    <strong>Выставка фотографов мехмата</strong>
+                </a>
+                <div class="navbar-collapse justify-content-evenly">
+                    <ul class="navbar-nav">
+                        <li class="nav-item px-xl-5">
+                            <a class="nav-link active" href="#">Лента</a>
+                        </li>
+                        <li class="nav-item px-lg-5">
+                            <a class="nav-link" href="#">Отбор</a>
+                        </li>
+                        <li class="nav-item px-xl-5">
+                            <a class="nav-link" href="#">Профиль</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
