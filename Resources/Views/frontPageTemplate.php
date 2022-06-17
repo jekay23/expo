64790313a62ac5a10bd7db84c3eceb566f7b36b1
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-17 15:56:43 +0700

    T-2.2 - Project structure - Add basic scrollable row

--- Resources/Views/frontPageTemplate.php
+++ Resources/Views/frontPageTemplate.php
@@ -1,3 +1,14 @@
-<main class="container" role="main" style="padding-top: 70px">
-    <div>Here you will be able to see the front page of Expo MMD website.</div>
+<main class="container-fluid" role="main" style="padding-top: 70px">
+    <div class="pt-3 pb-1">Текущая выставка</div>
+    <div class="scrolling-wrapper">
+
+        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
+        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
+    </div>
 </main>
