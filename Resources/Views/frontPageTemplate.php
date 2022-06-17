9c3b33263501ce09786d853d3beb0334f6a7f736
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-17 18:13:39 +0700

    T-2.8 - frontPageTemplate, Css - Improve scrollable row

--- Resources/Views/frontPageTemplate.php
+++ Resources/Views/frontPageTemplate.php
@@ -1,14 +1,32 @@
 <main class="container-fluid" role="main" style="padding-top: 70px">
-    <div class="pt-3 pb-1">Текущая выставка</div>
-    <div class="scrolling-wrapper">
+    <h2 class="pt-3 pb-1 ps-md-5">Текущая выставка</h2>
 
-        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/flowers.png" alt="Цветы"></div>
-        <div class="stack"><img src="/uploads/photos/mountains.png" alt="Цветы"></div>
+    <div class="scrolling-wrapper d-flex">
+        <div class="stack">
+            <h3 class="exhibit-title">Тема: <strong>Лето в Академгородке</strong></h3>
+            <p class="exhibit-descr">Новосибирский Академгородок — прекрасное место круглый год. Но так как лето в Сибири достаточно коротко, его атмосферой хочется пропитаться сполна, чтобы хватило на долгую зиму. Ждём ваших фото!</p>
+        </div>
+        <div class="stack">
+            <div class="one-of-stack d-flex justify-content-evenly">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+            </div>
+            <div class="one-of-stack d-flex justify-content-evenly">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+                <img src="/uploads/photos/mountains.png" alt="Цветы">
+                <img src="/uploads/photos/flowers.png" alt="Цветы">
+            </div>
+        </div>
     </div>
 </main>
