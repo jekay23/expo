d6bed74865c432953d8c0033a04b7623581e2967
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-20 18:21:08 +0700

    T-2.4 - smallGrid component - Add extra margin for header

--- Resources/Views/Components/smallGrid.php
+++ Resources/Views/Components/smallGrid.php
@@ -1,4 +1,4 @@
-<div class="container-fluid small-grid text-center">
+<div class="container-fluid small-grid text-center mb-2">
     <div class="small-grid-triple d-inline-flex">
         <img src="/uploads/photos/flowers.png" alt="Цветы">
         <img src="/uploads/photos/mountains.png" alt="Горы">
