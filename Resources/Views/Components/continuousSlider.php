429329b5f8d1ff44411bd202989a67beb2b6dec7
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-20 13:00:03 +0700

    T-2.2 - frontPageTemplate - Add continuous slider

---
+++ Resources/Views/Components/continuousSlider.php
@@ -0,0 +1,14 @@
+<div class="scrolling-wrapper d-flex">
+    <img src="/uploads/photos/flowers.png" alt="Цветы">
+    <img src="/uploads/photos/mountains.png" alt="Горы">
+    <img src="/uploads/photos/flowers.png" alt="Цветы">
+    <img src="/uploads/photos/mountains.png" alt="Горы">
+    <img src="/uploads/photos/flowers.png" alt="Цветы">
+    <img src="/uploads/photos/mountains.png" alt="Горы">
+    <img src="/uploads/photos/flowers.png" alt="Цветы">
+    <img src="/uploads/photos/mountains.png" alt="Горы">
+    <img src="/uploads/photos/flowers.png" alt="Цветы">
+    <img src="/uploads/photos/mountains.png" alt="Горы">
+    <img src="/uploads/photos/flowers.png" alt="Цветы">
+    <img src="/uploads/photos/mountains.png" alt="Горы">
+</div>
--- Resources/Views/frontPageTemplate.php
+++ Resources/Views/frontPageTemplate.php
@@ -1,8 +1,10 @@
 <main class="container-fluid" role="main" style="padding-top: 70px">
     <?php
     \Expo\Resources\Views\View::renderHeader2('Текущая выставка');
-
     require 'Components/exhibitionSlider.php';
+
+    \Expo\Resources\Views\View::renderHeader2('Лучшие фото месяца');
+    require 'Components/continuousSlider.php'
     ?>
 
 </main>
