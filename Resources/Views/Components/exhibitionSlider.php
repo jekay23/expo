661aa7a200fde52cbd3b601323a04fb9fd5a2e3b
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-20 12:41:28 +0700

    T-2.2 - frontPageTemplate - Move slider template to separate file

---
+++ Resources/Views/Components/exhibitionSlider.php
@@ -0,0 +1,28 @@
+<div class="scrolling-wrapper d-flex">
+    <div class="stack ps-md-3">
+        <h3 class="exhibit-title">Тема: <strong>Лето в Академгородке</strong></h3>
+        <p class="exhibit-descr">Новосибирский Академгородок — прекрасное место круглый год. Но так как лето в Сибири достаточно коротко, его атмосферой хочется пропитаться сполна, чтобы хватило на долгую зиму. Ждём ваших фото!</p>
+    </div>
+    <div class="stack">
+        <div class="one-of-stack d-flex justify-content-evenly">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+        </div>
+        <div class="one-of-stack d-flex justify-content-evenly">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+            <img src="/uploads/photos/mountains.png" alt="Цветы">
+            <img src="/uploads/photos/flowers.png" alt="Цветы">
+        </div>
+    </div>
+</div>
