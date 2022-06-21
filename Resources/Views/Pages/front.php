f8637646ee53672a290dc56dbdc3d366c0bc2df5
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-21 17:39:41 +0700

    T-2.6 - Html, Front templates - Move <main> formation to html

--- Resources/Views/Pages/front.php
+++ Resources/Views/Pages/front.php
@@ -1,27 +1,23 @@
-<main class="container-fluid" role="main" style="padding-top: 70px">
-    <?php
-    \Expo\Resources\Views\View::renderHeader2('Текущая выставка');
-    require __DIR__ . '/../Components/exhibitionSlider.php';
+<?php
+\Expo\Resources\Views\View::renderHeader2('Текущая выставка');
+require __DIR__ . '/../Components/exhibitionSlider.php';
 
-    \Expo\Resources\Views\View::renderHeader2('Лучшие фото месяца');
-    require __DIR__ . '/../Components/continuousSlider.php';
+\Expo\Resources\Views\View::renderHeader2('Лучшие фото месяца');
+require __DIR__ . '/../Components/continuousSlider.php';
 
-    \Expo\Resources\Views\View::renderHeader2('Последние опубликованные');
-    require __DIR__ . '/../Components/smallGrid.php';
+\Expo\Resources\Views\View::renderHeader2('Последние опубликованные');
+require __DIR__ . '/../Components/smallGrid.php';
 
-    \Expo\Resources\Views\View::renderHeader2('Предыдущая выставка');
-    require __DIR__ . '/../Components/continuousSlider.php';
+\Expo\Resources\Views\View::renderHeader2('Предыдущая выставка');
+require __DIR__ . '/../Components/continuousSlider.php';
 
-    \Expo\Resources\Views\View::renderHeader2('Фотографы мехмата');
-    $sliderText = 'photographers';
-    \Expo\App\Http\Controllers\Components\textSlider::renderSlider($sliderText);
+\Expo\Resources\Views\View::renderHeader2('Фотографы мехмата');
+$sliderText = 'photographers';
+\Expo\App\Http\Controllers\Components\textSlider::renderSlider($sliderText);
 
-    \Expo\Resources\Views\View::renderHeader2('Подборка команды мехмата');
-    require __DIR__ . '/../Components/carousel.php';
+\Expo\Resources\Views\View::renderHeader2('Подборка команды мехмата');
+require __DIR__ . '/../Components/carousel.php';
 
-    \Expo\Resources\Views\View::renderHeader2('Фильтры');
-    $sliderText = 'filters';
-    \Expo\App\Http\Controllers\Components\textSlider::renderSlider($sliderText);
-    ?>
-
-</main>
+\Expo\Resources\Views\View::renderHeader2('Фильтры');
+$sliderText = 'filters';
+\Expo\App\Http\Controllers\Components\textSlider::renderSlider($sliderText);
--- Resources/Views/html.php
+++ Resources/Views/html.php
@@ -9,10 +9,10 @@
     </head>
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-        <?php
-        require 'Components/header.php';
-        require $template;
-        require 'Components/footer.php';
-        ?>
+        <?php require 'Components/header.php'; ?>
+        <main class="container-fluid" role="main" style="padding-top: 70px">
+            <?php require $template; ?>
+        </main>
+        <?php require 'Components/footer.php'; ?>
     </body>
 </html>
