8042e1c8d548cb01fa614f87a01ea42e2aa273a2
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-23 15:33:17 +0700

    T-2.1 - Css, Header - Fix header to make it responsive

--- Resources/Views/Components/header.php
+++ Resources/Views/Components/header.php
@@ -1,31 +1,27 @@
 <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary" role="navigation"
-         style="height: 6vh; min-height: 60px">
-        <div class="container-md align-self-center">
-            <a class="navbar-brand py-0" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary" role="navigation">
+        <div class="container-fluid">
+            <a class="navbar-brand py-0 ps-5" href="https://www.nsu.ru/n/mathematics-mechanics-department/">
                 <img src="/image/logoMMD.png" alt="Логотип ММФ" style="height: 5vh; min-height: 55px">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <a class="navbar-brand px-lg-5" href="/">
                     <strong>Выставка фотографов мехмата</strong>
                 </a>
-                <div class="navbar-collapse justify-content-evenly">
-                    <ul class="navbar-nav">
-                        <li class="nav-item px-xl-5">
-                            <a class="nav-link active" href="/">Лента</a> <!-- TODO: Properly change active status-->
-                        </li>
-                        <li class="nav-item px-lg-5">
-                            <a class="nav-link" href="/exhibition">Отбор</a>
-                        </li>
-                        <li class="nav-item px-xl-5">
-                            <a class="nav-link" href="/sign-up">Профиль</a>
-                        </li>
-                    </ul>
-                </div>
+                <ul class="navbar-nav">
+                    <li class="nav-item px-xl-5">
+                        <a class="nav-link active" href="/">Лента</a> <!-- TODO: Properly change active status-->
+                    </li>
+                    <li class="nav-item px-lg-5">
+                        <a class="nav-link" href="/exhibition">Отбор</a>
+                    </li>
+                    <li class="nav-item px-xl-5 pe-5">
+                        <a class="nav-link" href="/sign-up">Профиль</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
