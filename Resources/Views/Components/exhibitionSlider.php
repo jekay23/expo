c7e2a2a73b3b667f2c474619db2b3ab001618f05
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-23 18:37:20 +0700

    T-2.2 - Css - Make all photos in exhibition slider 3:2

--- Resources/Views/Components/exhibitionSlider.php
+++ Resources/Views/Components/exhibitionSlider.php
@@ -3,7 +3,7 @@
         <h3 class="exhibit-title">Тема: <strong>Лето в Академгородке</strong></h3>
         <p class="exhibit-descr">Новосибирский Академгородок — прекрасное место круглый год. Но так как лето в Сибири достаточно коротко, его атмосферой хочется пропитаться сполна, чтобы хватило на долгую зиму. Ждём ваших фото!</p>
     </div>
-    <div class="stack">
+    <div class="stack mmd-exhibition">
         <div class="one-of-stack d-flex justify-content-evenly">
             <img src="/uploads/photos/flowers.jpg" alt="Цветы">
             <img src="/uploads/photos/wall.png" alt="Стена">
