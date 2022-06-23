0917e4586803de9c0a3f7d6d346171549bffbf2f
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-23 18:52:36 +0700

    T-2.1 - Css, Footer - Enhance footer responsiveness

--- Resources/Views/Components/footer.php
+++ Resources/Views/Components/footer.php
@@ -1,9 +1,9 @@
 <footer class="footer mt-5 py-3 bg-secondary">
     <div class="container">
-        <ul class="nav justify-content-center">
-            <li class="nav-item px-5"><a href="#" class="nav-link text-muted">Поддержка</a></li>
-            <li class="nav-item px-5"><a href="#" class="nav-link text-muted">Пользовательское соглашение</a></li>
-            <li class="nav-item px-5"><a href="#" class="nav-link text-muted">FAQ</a></li>
+        <ul class="nav justify-content-center text-center">
+            <li class="nav-item"><a href="#" class="nav-link text-muted">Поддержка</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-muted">Пользовательское соглашение</a></li>
+            <li class="nav-item"><a href="#" class="nav-link text-muted">FAQ</a></li>
         </ul>
     </div>
 </footer>
