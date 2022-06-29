42e95041f5f746cb58a439a57923c343b7224af7
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-29 18:59:36 +0700

    T-3.4 - Model - Beautify and improve queries for most liked photos

--- App/Models/DataBaseConnection.php
+++ App/Models/DataBaseConnection.php
@@ -32,52 +32,41 @@ class DataBaseConnection
 
     public static function requirePhotos(string $type, int $quantity, array $args = null): array
     {
-        if (null == self::$connection) {
-            $connectionAttempt = self::open();
-            if (!$connectionAttempt[0]) {
-                return array(false, null);
-            }
+        if (!self::makeSureConnectionIsOpen()) {
+            return array(false, null);
         }
 
         $query = "SELECT location, altText FROM Photos";
         if ('latest' == $type) {
             $query .= " ORDER BY uploadTime DESC";
-        } elseif ('best' == $type) {
-            $query .= " ORDER BY likes DESC, uploadTime DESC";
         } elseif ('compilation' == $type) {
             if (isset($args['compilationID'])) {
                 $compilationID = $args['compilationID'];
                 $query .= " RIGHT JOIN CompilationItems CI ON Photos.photoID = CI.photoID";
                 $query .= " WHERE compilationID = $compilationID";
             }
+        } elseif ('best' == $type) {
+            $query = 'SELECT location, altText, COUNT(userID) AS likes, uploadTime ' .
+                'FROM Photos P RIGHT JOIN Likes L ON P.photoID = L.photoID GROUP BY P.photoID ORDER BY likes DESC';
         }
         $query .= " LIMIT $quantity";
 
         // TODO: now if none of the conditions are met, the query just asks for $quantity photos, have to think about it
 
-        $statement = self::$connection->prepare($query);
-        $statement->execute();
-
-        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $statement = self::executeStatement($query);
         $photos = $statement->fetchAll();
         return array(true, $photos);
     }
 
     public static function requireCompilationDetails(int $compilationID): array
     {
-        if (null == self::$connection) {
-            $connectionAttempt = self::open();
-            if (!$connectionAttempt[0]) {
-                return array(false, null);
-            }
+        if (!self::makeSureConnectionIsOpen()) {
+            return array(false, null);
         }
 
         $query = "SELECT name, description FROM Compilations WHERE compilationID=$compilationID";
 
-        $statement = self::$connection->prepare($query);
-        $statement->execute();
-
-        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $statement = self::executeStatement($query);
         $exhibitions = $statement->fetchAll();
         return array(true, $exhibitions[0]);
     }
@@ -86,40 +75,52 @@ class DataBaseConnection
     {
         if ('filters' == $type) {
             $filters = array(
-                array('name' => 'По дате публикации'),
-                array('name' => 'По поулярности'),
-                array('name' => 'По выставкам')
+                array('name' => 'По дате публикации', 'link' => ''),
+                array('name' => 'По поулярности', 'link' => ''),
+                array('name' => 'По выставкам', 'link' => '')
             );
             return array(true, $filters);
         }
-        if (null == self::$connection) {
-            $connectionAttempt = self::open();
-            if (!$connectionAttempt[0]) {
-                return array(false, null);
-            }
+
+        if (!self::makeSureConnectionIsOpen()) {
+            return array(false, null);
         }
 
         $query = "SELECT name FROM Users";
         if ('latest' == $type) {
             $query .= " ORDER BY signUpDate DESC";
-        } elseif ('best' == $type) {
-            $query .= " ORDER BY likes DESC, uploadTime DESC";
         } elseif ('compilation' == $type) {
             if (isset($args['compilationID'])) {
                 $compilationID = $args['compilationID'];
-                $query .= " RIGHT JOIN CompilationItems CI ON Photos.photoID = CI.photoID";
-                $query .= " WHERE compilationID = $compilationID";
+                $query .= ''; // TODO
             }
+        } elseif ('best' == $type) {
+            $query .= ''; // TODO
         }
         $query .= " LIMIT $quantity";
 
-        // TODO: now if none of the conditions are met, the query just asks for $quantity photos, have to think about it
+        $statement = self::executeStatement($query);
+        $users = $statement->fetchAll();
+        return array(true, $users);
+    }
 
+    private static function makeSureConnectionIsOpen(): bool
+    {
+        if (null == self::$connection) {
+            $connectionAttempt = self::open();
+            if (!$connectionAttempt[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static function executeStatement(string $query): \PDOStatement
+    {
         $statement = self::$connection->prepare($query);
         $statement->execute();
 
         $statement->setFetchMode(PDO::FETCH_ASSOC);
-        $users = $statement->fetchAll();
-        return array(true, $users);
+        return $statement;
     }
 }
