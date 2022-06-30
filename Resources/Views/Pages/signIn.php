0c7d94eaa1f0c6e8ff2010bf18d12ca5dc75c5b5
Evgeny Yatsko <evgyatsko@gmail.com>
2022-06-30 15:25:02 +0700

    T-3.6 - signIn, signUp - Add sign-in page and enchance sign-up page

--- Resources/Views/Pages/signIn.php
+++ Resources/Views/Pages/signIn.php
@@ -1,1 +1,21 @@
-Here you will be able to see the sign-in page.
+<?php $stickFooter = true; ?>
+<div class="text-center pt-5 pb-3">
+    <h1><strong>Войдите в ваш аккаунт</strong></h1>
+</div>
+<div class="container-fluid text-center mmd-sign-in">
+    <div class="row">
+        <p class="d-inline-flex">Email</p>
+        <div class="d-inline-flex text-start mmd-input-wrap"><input></div>
+        <p class="d-inline-flex"> </p>
+    </div>
+    <div class="row">
+        <p class="d-inline-flex">Пароль</p>
+        <div class="d-inline-flex text-start mmd-input-wrap"><input type="password"></div>
+        <p class="d-inline-flex"> </p>
+    </div>
+    <button type="submit">
+        Войти
+    </button>
+    <p><a href="/">Восстановить пароль</a></p>
+    <p><a href="/sign-up">Зарегестрироваться</a></p>
+</div>
--- Resources/Views/Pages/signUp.php
+++ Resources/Views/Pages/signUp.php
@@ -1,3 +1,4 @@
+<?php $stickFooter = true; ?>
 <div class="text-center pt-5 pb-3">
     <h1><strong>Зарегестрируйтесь</strong></h1>
 </div>
@@ -19,7 +20,17 @@
     </div>
     <div class="row">
         <p class="d-inline-flex">Обращение</p>
-        <div class="d-inline-flex text-start mmd-input-wrap"><input></div>
+        <div class="d-inline-flex text-start mmd-input-wrap">
+            <select class="form-select mmd-select" name="pronoun" id="pronounChoice">
+                <option value="none" selected>—</option>
+                <option value="he">он</option>
+                <option value="she">она</option>
+            </select>
+        </div>
         <p class="d-inline-flex"> </p>
     </div>
+    <button type="submit">
+        Зарегестрироваться
+    </button>
+    <p><a href="/sign-in">У меня есть профиль</a></p>
 </div>
