0d9d3ca1e7a10079a12a88a5c196e83a11561393
Evgeny Yatsko <evgyatsko@gmail.com>
2022-07-01 15:23:24 +0700

    T-3.8 - Autoloader - Require autoloaded classes once

--- Routes/autoloader.php
+++ Routes/autoloader.php
@@ -12,12 +12,9 @@ spl_autoload_register(function ($class) {
 
     $relativeClass = substr($class, $len);
 
-    // replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
     $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
     if (file_exists($file)) {
-        require $file;
+        require_once $file;
     }
 });
