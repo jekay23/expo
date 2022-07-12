fef7ad18642cb75c6103eae52eecec9e59650e9c
Evgeny Yatsko <evgyatsko@gmail.com>
2022-07-12 12:05:28 +0700

    T-4.5 - SmallGrid - Make grids adjustable

--- Resources/Views/Components/SmallGrid.php
+++ Resources/Views/Components/SmallGrid.php
@@ -6,15 +6,27 @@ use Expo\Resources\Views\View;
 
 class SmallGrid
 {
-    public static function render(string $headerText, array $photos)
+    public static function render(string $headerText, array $photos, int $photosPerBlock = 3)
     {
-        if (0 === count($photos) % 6) {
-            $numOfTriples = count($photos) / 3;
-            $triples = [];
-            for ($i = 0; $i < $numOfTriples; $i++) {
-                $triples[$i] = [$photos[3 * $i], $photos[3 * $i + 1], $photos[3 * $i + 2]];
+        // the last block may not be full
+        $numOfBlocks = intdiv(count($photos) + ($photosPerBlock - 1), $photosPerBlock);
+        $blocks = [];
+        // fill all the blocks except for the last one
+        for ($i = 0; $i < ($numOfBlocks - 1); $i++) {
+            $blocks[$i] = [];
+            for ($j = 0; $j < $photosPerBlock; $j++) {
+                $blocks[$i][] = $photos[$photosPerBlock * $i + $j];
             }
-            View::requireTemplate('smallGrid', 'Component', compact('headerText', 'triples'));
         }
+        // fill the last block
+        $blocks[$numOfBlocks - 1] = [];
+        for ($j = 0; $j < $photosPerBlock; $j++) {
+            if (isset($photos[$photosPerBlock * ($numOfBlocks - 1) + $j])) {
+                $blocks[$numOfBlocks - 1][] = $photos[$photosPerBlock * ($numOfBlocks - 1) + $j];
+            } else {
+                break;
+            }
+        }
+        View::requireTemplate('smallGrid', 'Component', compact('headerText', 'blocks'));
     }
 }
--- Resources/Views/Components/Templates/smallGrid.php
+++ Resources/Views/Components/Templates/smallGrid.php
@@ -1,9 +1,9 @@
 <hr>
 <h2><?= $headerText ?></h2>
 <div class="container-fluid small-grid text-center mb-2">
-    <?php foreach ($triples as $triple) : ?>
+    <?php foreach ($blocks as $block) : ?>
         <div class="small-grid-triple d-inline-flex">
-            <?php foreach ($triple as $photo) : ?>
+            <?php foreach ($block as $photo) : ?>
                 <img src="<?= '/uploads/photos/' . $photo['location'] ?>" alt="<?= $photo['altText'] ?>">
             <?php endforeach; ?>
         </div>
