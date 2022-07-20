e7307f09422d5d2e8018b8920a90ccdcb26d7b51
Evgeny Yatsko <evgyatsko@gmail.com>
2022-07-20 13:00:30 +0700

    T-6.6 - changePasswordEmail - Spread out input fields

--- Resources/Views/Pages/Templates/changePasswordEmail.php
+++ Resources/Views/Pages/Templates/changePasswordEmail.php
@@ -11,13 +11,18 @@ use Expo\Resources\Views\Components\TextField;
     </div>
     <div class="mt-3 col-12 col-lg-7 mmd-edit-profile">
         <form method="post" target="_self" action="/api/change-password-email">
+            <h3 class="mb-3">Изменить email</h3>
             <?php TextField::render('changePasswordEmail', 'Email', ['name' => 'email', 'value' => $email]); ?>
-            <?php TextField::render('changePasswordEmail', 'Старый пароль', ['name' => 'oldPassword', 'type' => 'password']); ?>
+            <hr>
+            <h3 class="mt-4 mb-3">Изменить пароль</h3>
             <?php TextField::render('changePasswordEmail', 'Новый пароль', ['name' => 'newPassword', 'type' => 'password']); ?>
             <?php TextField::render('changePasswordEmail', 'Новый пароль ещё раз', ['name' => 'newPasswordAgain', 'type' => 'password']); ?>
-            <div class="row justify-content-center">
-                <div class="col-6 mmd-edit-wrap d-flex justify-content-center justify-content-sm-start p-0 px-sm-3">
-                    <button class="mmd-thin-button mt-0" type="submit">Сохранить</button>
+            <hr>
+            <h3 class="mt-4 mb-3">Подтвердить действие и сохранить</h3>
+            <?php TextField::render('changePasswordEmail', 'Старый пароль', ['name' => 'oldPassword', 'type' => 'password', 'required' => true]); ?>
+            <div class="row justify-content-center mb-5">
+                <div class="col-6 mmd-edit-wrap d-flex justify-content-center justify-content-sm-start p-0 px-sm-3 mb-5">
+                    <button class="mmd-thin-button mt-0 mb-5" type="submit">Сохранить</button>
                 </div>
             </div>
         </form>
