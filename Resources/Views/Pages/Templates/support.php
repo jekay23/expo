5678d1327e22023d0c0791b515e1bb8e495f6eb6
Evgeny Yatsko <evgyatsko@gmail.com>
2022-08-03 14:06:31 +0700

    T-7.1,2,3 - Page templates, Css - Fill EULA, FAQ, Support

--- Resources/Views/Pages/Templates/support.php
+++ Resources/Views/Pages/Templates/support.php
@@ -1,1 +1,16 @@
-<h1>Контакты</h1>
+<div class="mt-3 text-center mmd-faq">
+    <h1><strong>Поддержка</strong></h1>
+    <div class="row justify-content-center text-start">
+        <div class="col-11 col-md-8 col-lg-7">
+            Если у вас возникли следующие вопросы или трудности:
+            <ul class="mmd-list">
+                <li>трудности с аутентификацией — входом в аккаунт или регистрацией,</li>
+                <li>проблемы при редактировании профиля,</li>
+                <li>некорректная работа интерфейса сайта,</li>
+                <li>трудности с загрузкой фотографий,</li>
+                <li>предложения о сотрудничестве,</li>
+            </ul>
+            пожалуйста, обращайтесь к администрации сайта по почте <a href="mailto:eugene_yatsko@dotwrk.com">eugene_yatsko@dotwrk.com</a>.
+        </div>
+    </div>
+</div>
